af1625299476add3e011b071576bb22755d547fe
Carlo Sala <carlosalag@protonmail.com>
2021-12-02 11:17:09 +0100

    feat: day 02

---
+++ 02/day02.ts
@@ -0,0 +1,45 @@
+import input from "./input.ts"
+
+function part1(i: Array<string>) {
+  let xPos = 0
+  let depth = 0
+  i.forEach((act) => {
+    const splAct = act.split(" ")
+    const num = Number(splAct[1])
+    if (splAct[0] === "up") {
+      depth -= num
+      return
+    }
+    if (splAct[0] === "down") {
+      depth += num
+      return
+    }
+    xPos += num
+  })
+  return xPos * depth
+}
+
+function part2(i: Array<string>) {
+  let xPos = 0
+  let depth = 0
+  let aim = 0
+  i.forEach((act) => {
+    const splAct = act.split(" ")
+    const num = Number(splAct[1])
+    if (splAct[0] === "up") {
+      aim -= num
+      return
+    }
+    if (splAct[0] === "down") {
+      aim += num
+      return
+    }
+    xPos += num
+    depth += aim * num
+  })
+  return xPos * depth
+}
+
+const inputArray = input.split("\n")
+console.log("Part 1: " + part1(inputArray))
+console.log("Part 2: " + part2(inputArray))
