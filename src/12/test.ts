dbd58289cb0842c481633bfbef465989bd44eb67
Carlo Sala <carlosalag@protonmail.com>
2021-12-12 14:01:25 +0100

    feat: day 12

---
+++ src/12/test.ts
@@ -0,0 +1,18 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts"
+import { part1, part2, parseInput } from "./day12.ts"
+
+const input = parseInput("input.test.txt")
+
+Deno.test({
+  name: "Day 12, part 1",
+  fn: () => {
+    assertEquals(part1(input), 19)
+  },
+})
+
+Deno.test({
+  name: "Day 12, part 2",
+  fn: () => {
+    assertEquals(part2(input), 103)
+  },
+})
