a2515021a2de58f9020a3090ba7d74d2529ab7d5
Carlo Sala <carlosalag@protonmail.com>
2021-12-21 17:49:28 +0100

    fix: removing useless code

--- src/downloadInput.ts
+++ src/downloadInput.ts
@@ -2,10 +2,6 @@ import { inputPath } from "./utils.ts";
 import "https://deno.land/x/dotenv/load.ts";
 
 async function downloadInput(day: string) {
-  if (Deno.args.length !== 1) {
-    console.error("You must pass the day as an argument!");
-    return 1;
-  }
   const url = `https://adventofcode.com/2021/day/${day}/input`;
   const headers = new Headers({
     Cookie: `session=${Deno.env.get("AUTH_COOKIE")}`,
